a5f7ac588997eff2f784fc0b24ced76496a6ce04
Tomasz Sterna <tomasz@sterna.link>
2023-07-30 17:08:36 +0200

    Build VIC-20 as X65 base

---
+++ emulator/apps/x65-ui.cc
@@ -0,0 +1,33 @@
+/*
+    UI implementaion for x65.c
+*/
+#include "chips/chips_common.h"
+#include "chips/m6502.h"
+#include "chips/m6522.h"
+#include "chips/m6561.h"
+#include "chips/kbd.h"
+#include "chips/mem.h"
+#include "chips/clk.h"
+#include "systems/c1530.h"
+#include "systems/x65.h"
+#define UI_DASM_USE_M6502
+#define UI_DBG_USE_M6502
+#define CHIPS_UTIL_IMPL
+#include "util/m6502dasm.h"
+#define CHIPS_UI_IMPL
+#include "imgui.h"
+#include "util/m6502dasm.h"
+#include "ui/ui_util.h"
+#include "ui/ui_chip.h"
+#include "ui/ui_memedit.h"
+#include "ui/ui_memmap.h"
+#include "ui/ui_dasm.h"
+#include "ui/ui_dbg.h"
+#include "ui/ui_m6502.h"
+#include "ui/ui_m6522.h"
+#include "ui/ui_m6561.h"
+#include "ui/ui_audio.h"
+#include "ui/ui_kbd.h"
+#include "ui/ui_c1530.h"
+#include "ui/ui_snapshot.h"
+#include "ui/ui_x65.h"
